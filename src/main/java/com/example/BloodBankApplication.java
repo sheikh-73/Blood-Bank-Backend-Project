0d1b03938773f4e29ac6e1c28fc1526afae09628
sheikh-73 <146777656+sheikh-73@users.noreply.github.com>
2024-06-07 22:40:13 +0600

    Add files via upload

---
+++ src/main/java/com/example/BloodBankApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class BloodBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodBankApplication.class, args);
+		System.out.println("Start...");
+	}
+
+}
